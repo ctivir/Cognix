856c7397061c9f334ba1d3209cdc01b9b7a1bfc2
marcos <marcos@2c3bab08-efa0-4942-86ea-8f028fc1a923>
2013-06-03 18:24:06 +0000

    movendo para o trunk

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Executando o script de inicialização da base de dados..."
+sudo -u postgres psql -c "CREATE USER cognitiva WITH PASSWORD 'rep@cognitiva'"
+sudo -u postgres dropdb repositorio
+sudo -u postgres createdb -O cognitiva repositorio
+
+sudo -u postgres psql repositorio -f ./dataBase/schema.sql
+
+echo "Criando a pasta para o log"
+sudo mkdir /var/log/cognitiva
+sudo chown tomcat7:tomcat7 /var/log/cognitiva/
+
+echo "Criando a pasta onde serão salvos os objetos educacionais"
+sudo mkdir -p /var/cognitiva/repositorio/
+sudo chown -R tomcat7:tomcat7 /var/cognitiva/
