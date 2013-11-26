35d028bc44839ac9cf5747a5a6e71827dc9ba62c
marcos <marcos@2c3bab08-efa0-4942-86ea-8f028fc1a923>
2013-11-26 19:13:39 +0000

    REPOSITORIO-181 Teste para Message

---
+++ src/test/java/com/cognitivabrasil/repositorio/util/MessageTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.cognitivabrasil.repositorio.util;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+
+/**
+ *
+ * @author Marcos Freitas Nunes <marcos@cognitivabrasil.com.br>
+ */
+public class MessageTest {
+    private final String MSG = "Mensagem de erro!";
+    private final String HREF = "www.marcos.nunes";
+    
+    @Test
+    public void testContrutor(){
+        Message msg = new Message(Message.ERROR, MSG, HREF);
+        assertThat(msg.getType(), equalTo(Message.ERROR));
+        assertThat(msg.getMessage(), equalTo(MSG));
+        assertThat(msg.getHref(), equalTo(HREF));        
+    }
+    
+    @Test
+    public void testSetter(){
+        Message msg = new Message();
+        msg.setType(Message.ERROR);
+        msg.setMessage(MSG);
+        msg.setHref(HREF);
+        
+        assertThat(msg.getType(), equalTo(Message.ERROR));
+        assertThat(msg.getMessage(), equalTo(MSG));
+        assertThat(msg.getHref(), equalTo(HREF)); 
+    }
+    
+}
