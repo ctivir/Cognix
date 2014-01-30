2514b19b1531e2759cacc9a08cc4651c27205a91
preto <preto@2c3bab08-efa0-4942-86ea-8f028fc1a923>
2014-01-30 17:53:25 +0000

    correções do sonar: preservação do stack trace

--- src/main/java/metadata/conversor/Rule.java
+++ src/main/java/metadata/conversor/Rule.java
@@ -11,56 +11,60 @@ import org.apache.log4j.Logger;
 /**
  *
  * @author paulo
- * 
+ *
  * Classe para criar regras de conversão.
  */
 public class Rule {
-	private String from;
-	private String to;
-        private Logger log;
 
-	/**
-	 * 
-	 * @param from 
-	 * @param to
-	 */
-	Rule(String f, String t)	{
-		from = f;
-		to = t;
-		
-	}
-/**
-	 * 
-	 * @param objeto1 Objeto origem da transformacao. Neste objeto será chamada a função "get" + from
-	 * @param objeto2 Objeto destino. Neste objeto será chamado o método "set" + from, com o resultado
-	 * do get do objeto 1
-	 * @throws IllegalArgumentException Caso um dos métodos não exista.
-	 */
-	public void apply(Object ob1, Object ob2) throws IllegalArgumentException {
-		Class c1 = ob1.getClass();
-		Class c2 = ob2.getClass();
-		Method m1, m2;
+    private String from;
+    private String to;
+    private Logger log;
 
-		try { 
-			m1 = c1.getMethod("get" + from);
-		} catch(NoSuchMethodException e) {
-			throw new IllegalArgumentException("Cannot find method " + "get" + from 
-				+ " in class" + c1.getName());
-		}
+    /**
+     *
+     * @param from
+     * @param to
+     */
+    Rule(String f, String t) {
+        from = f;
+        to = t;
+        
+    }
 
-		try {
-			m2 = c2.getMethod("set" + to, m1.getReturnType());
-		} catch(NoSuchMethodException e) {
-			throw new IllegalArgumentException("Cannot find method " + "set" + from 
-				+ " in class" + c2.getName());
-		}
-		
-
-		try {
-			m2.invoke(ob2, m1.invoke(ob1));
-		}
-		catch(IllegalArgumentException | InvocationTargetException | IllegalAccessException e) {
-                    log.error(e);			
-		}
-	}
+    /**
+     *
+     * @param objeto1 Objeto origem da transformacao. Neste objeto será chamada
+     * a função "get" + from
+     * @param objeto2 Objeto destino. Neste objeto será chamado o método "set" +
+     * from, com o resultado do get do objeto 1
+     * @throws IllegalArgumentException Caso um dos métodos não exista.
+     */
+    public void apply(Object ob1, Object ob2) throws IllegalArgumentException {
+        Class c1 = ob1.getClass();
+        Class c2 = ob2.getClass();
+        Method m1, m2;
+        
+        try {            
+            m1 = c1.getMethod("get" + from);
+        } catch (NoSuchMethodException e) {
+            log.error(e);
+            throw new IllegalArgumentException("Cannot find method " + "get" + from
+                    + " in class" + c1.getName());
+        }
+        
+        try {
+            m2 = c2.getMethod("set" + to, m1.getReturnType());
+        } catch (NoSuchMethodException e) {
+            log.error(e);
+            throw new IllegalArgumentException("Cannot find method " + "set" + from
+                    + " in class" + c2.getName());
+        }
+        
+        
+        try {
+            m2.invoke(ob2, m1.invoke(ob1));
+        } catch (IllegalArgumentException | InvocationTargetException | IllegalAccessException e) {
+            log.error(e);            
+        }
+    }
 }
