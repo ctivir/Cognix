e3ccbdf5549bb86d9472c5045bba67a3850fc158
marcos <marcos@2c3bab08-efa0-4942-86ea-8f028fc1a923>
2014-02-05 20:06:26 +0000

    Coloquei botão no sair.
    Removi o listDocuments.js do do listDocuments.jspx
    
    Diminui o tamanho dos botoes btn-lg
    
    Arrumei o editar do show documents

--- src/main/webapp/scripts/listDocuments.js
+++ src/main/webapp/scripts/listDocuments.js
@@ -29,7 +29,7 @@ $(function() {
         }, "json")
                 .error(function(jqxhr) {
             $('#confirmModal').modal('hide');
-            if (jqxhr.status == 403) {
+            if (jqxhr.status === 403) {
                 $("#errorThrown").text("Você não possui permissão para deletar esse arquivo!");
             } else {
                 $("#errorThrown").text("Não foi possível executar a operação!");
@@ -40,26 +40,5 @@ $(function() {
       return false;
        
    });
-                               
-    $('.relation_link')
-    .button({
-        icons: {
-            primary: "ui-icon-triangle-1-s"
-        }
-    })
-    .click(function(){
-        var caixa = $(this).siblings('.relation_dropdown');
-        caixa.toggle()
-        .addClass("dropDownBox");
-        
-        var btnLeft= $(this).offset().left;
-        var btnTop = $(this).offset().top + $('.relation_link').outerHeight();
-        var btnWidth = $(this).outerWidth();                    
-        
-        caixa.css({                        
-            'left': btnLeft,
-            'top': btnTop,                        
-            'width': btnWidth                        
-        })
-    });
+
 });
