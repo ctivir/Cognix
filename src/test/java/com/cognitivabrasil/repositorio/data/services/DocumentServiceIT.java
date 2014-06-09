051f0133bb8e44a27a94f3e639b5b0c28c16b610
Marcos Nunes <marcosn@gmail.com>
2014-06-09 16:27:46 -0300

    Alterações para correção de importações não utilizadas e coisas do tipo

--- src/test/java/com/cognitivabrasil/repositorio/data/services/DocumentServiceIT.java
+++ src/test/java/com/cognitivabrasil/repositorio/data/services/DocumentServiceIT.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
